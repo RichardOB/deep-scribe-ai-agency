fbcf9991bd57e67a82d10459b3eab90bdbc55188
Richard OBrien <richard@jemhr.com>
2025-02-09 16:19:34 +0200

    Fetch Tavily and Open AI keys from user if not set from .env

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import os
+from getpass import getpass
+
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+
+if not os.getenv('OPENAI_KEY'):
+    os.environ['OPENAI_API_KEY'] = getpass('Enter Open AI API Key: ')
+
+
+if not os.getenv('TAVILY_API_KEY'):
+    os.environ['TAVILY_API_KEY'] = getpass('Enter Tavily Search API Key: ')
 
 
 if __name__ == '__main__':
